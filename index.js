360114b4b1eaccce8eed7c1a527b24c6de3885c2
chudaa203 <chudaa203@gmail.com>
2023-08-24 11:20:15 -0400

    Added some JavaScript functions

--- index.js
+++ index.js
@@ -30,9 +30,27 @@ const hiddenContent = document.getElementById("hiddenContent");
 showButton.addEventListener("click", function () {
   if (hiddenContent.style.display === "none") {
     hiddenContent.style.display = "block";
-    showButton.textContent = "Hide Certificate";
+    showButton.textContent = "Hide CIW Certificate";
   } else {
     hiddenContent.style.display = "none";
-    showButton.textContent = "Show Certificate";
+    showButton.textContent = "Show CIW Certificate";
   }
+});
+
+// contact me (form)
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Get form values
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
+
+  // Display an alert with the form data
+  const formData = `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+  alert(formData);
+
+  // Reset the form
+  this.reset();
 });
